6449ecbafd9df8c76ce170aacad2dabf9428d4a7
lrd7512369 <724441613@qq.com>
2017-04-05 15:45:05 +0800

    111
    111

---
+++ src/main/java/com/lrd/loadingforresult/OnLoadingResultListener.java
@@ -0,0 +1,17 @@
+package com.lrd.loadingforresult;
+
+/**
+ * Created by lierdong on 2017/4/5 上午9:36
+ */
+
+public interface OnLoadingResultListener {
+
+    void onFillRingStart();
+
+    void onFillRingEnd();
+
+    void onResultShowStart();
+
+    void onResultShowEnd();
+
+}
